3c36a73f322cb5aa53b1840d3729ada5bf5b2544
yusuga <dev@yusuga.com>
2023-08-18 16:54:15 +0900

    chore: add NFCTagIO.xcodeproj

---
+++ App/iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFCTagIO
+//
+//  Created by yusuga on 2023/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/iOS/NFCTagIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCTagIOApp.swift
+//  NFCTagIO
+//
+//  Created by yusuga on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct NFCTagIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
